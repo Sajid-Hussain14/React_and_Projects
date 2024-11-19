cf07ba059820696cb84c9ef6bc74c629384cbdc5
SAJID HUSSAIN GANIE <96110929+Sajid-Hussain14@users.noreply.github.com>
2024-11-19 04:29:22 -0800

    Add files via upload

---
+++ contextAPI/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import UserContextProvider from './context/UserContextProvider.jsx'
+import Login from './components/Login.jsx'
+import Profile from './components/Profile.jsx'
+
+function App() {
+
+
+  return (
+    <UserContextProvider >
+     <h1>Context API by SAJID</h1>
+     <Login />
+     <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
